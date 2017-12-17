4038fe576c4e88ea53f38a5a168353ed46a08963
xixirock <jlin0402@gmail.com>
2017-12-17 05:13:27 -0800

    added product table and department table

--- Bamazon.sql
+++ Bamazon.sql
@@ -11,3 +11,13 @@ CREATE TABLE products(
     cost FLOAT(8),
     PRIMARY KEY(item_id)
 );
+
+CREATE TABLE departments(
+	department_id INTEGER AUTO_INCREMENT NOT NULL,
+	department_name VARCHAR(50) NOT NULL,
+	over_head_costs FLOAT(8) NOT NULL,
+	product_sales FLOAT(8) DEFAULT 0,
+	product_cost FLOAT(8) DEFAULT 0,
+	total_profit FLOAT(8) DEFAULT 0,
+	PRIMARY KEY(department_id)
+);
